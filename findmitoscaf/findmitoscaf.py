4dc1fb5fb6d1654aee7fda91174826bfe732b709
Prunoideae <a455167189@163.com>
2019-12-12 23:26:27 +0800

    LICENSE

--- findmitoscaf/findmitoscaf.py
+++ findmitoscaf/findmitoscaf.py
@@ -20,3 +20,15 @@ You should have received a copy of the GNU General Public License
 along with MitoX.  If not, see <http://www.gnu.org/licenses/>.
 
 """
+
+import os
+import sys
+try:
+    sys.path.insert(0, os.path.abspath(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..")))
+    from helper import shell_call, direct_call
+except Exception as identifier:
+    print("Unable to import helper module, is the installation of MitoX valid?")
+
+def findmitoscaf():
+    pass
