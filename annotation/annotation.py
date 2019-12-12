fde153ab82bb486f4e1613ea46dce61fbd37d049
Prunoideae <a455167189@163.com>
2019-12-12 23:22:20 +0800

    Something

---
+++ annotation/annotation.py
@@ -0,0 +1,22 @@
+"""
+annotation.py
+=========
+
+Copyright (c) 2019-2020 Li Junyu <2018301050@szu.edu.cn>.
+
+This file is part of MitoX.
+
+MitoX is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MitoX is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MitoX.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
